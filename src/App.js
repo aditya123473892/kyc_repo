dd495b881eba34ffe587d38c48198d4b49b37164
Anuj Mishra <anujmishraan2005@gmail.com>
2023-09-14 14:22:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Routes,Route,useNavigationType,useLocation,} from "react-router-dom";
 // import Navbar from './Components/Navbar'
 import Home from './Components/Home';
 import Footer from './Components/Footer'
-import { Lab } from './Screens/About';
+
 import Updates from './Components/home-components/Updates'
 import Navbar from './Components/Navbar'
 function App() {
@@ -11,7 +11,6 @@ function App() {
     <div>
        <Navbar></Navbar> 
       <Home></Home>
-      <Lab></Lab>
       <Updates></Updates>
       <Footer></Footer>
       </div>
