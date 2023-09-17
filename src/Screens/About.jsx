c263e2cb87ffa51e6534ca0efdc2388dc37ffcb9
Anuj Mishra <anujmishraan2005@gmail.com>
2023-09-18 01:30:12 +0530

    New Commit

--- src/Screens/About.jsx
+++ src/Screens/About.jsx
@@ -1,1 +1,59 @@
+import React from "react";
+import "./About.css";
+
+export const Lab = () => {
+  return (
+    <div className="lab">
+      <p className="heading-works" style={{ fontWeight: 'bold' , fontSize: '3vw' }}>TOP B.TECH^(COURSES FILTER) COLLEGES OF INDIA</p>
+      <br></br>
+      <div className="frame">
+      <div className="div">
+        <div className="rectangle"/>
+        <div className="img"  />
+        <div className="rectangle-2"  />
+        <div className="rectangle-3" />
+        <div className="rectangle-4" />
+        <div className="rectangle-5" />
+        <div className="rectangle-6" />
+        <div className="rectangle-7"  />
+        <div className="text-wrapper">BITS</div>
+        <div className="text-wrapper-2">MIT</div>
+        <div className="text-wrapper-3">LNMIIT</div>
+        <div className="text-wrapper-4">BMSCE</div>
+        <div className="text-wrapper-5">DAIICT</div>
+        <div className="text-wrapper-6">THAPAR</div>
+        <div className="text-wrapper-7">M.S.EAMAIAH</div>
+        <div className="text-wrapper-8">R.V.COLLEGE</div>
+      </div>
+    </div>
+
+    <p className="images" style={{ fontWeight: 'bold' , fontSize: '3vw' }}><br></br>TOP ENGINEERING^(COURSE FILTER) COLLEGES OF UTTAR PRADESH^(STATES FILTER)</p>
+    <br></br>
+    <div className="frame1">
+      <div className="div1">
+        <div className="rectangle1" />
+        <div className="img1" />
+        <div className="rectangle-21"  />
+        <div className="rectangle-31" />
+        <div className="rectangle-41" />
+        <div className="rectangle-51" />
+        <div className="rectangle-61" />
+        <div className="rectangle-71" />
+        <div className="text-wrapper1">HBTU</div>
+        <div className="text-wrapper-21">JIIT</div>
+        <div className="text-wrapper-31">SNU</div>
+        <div className="text-wrapper-41">JSS</div>
+        <div className="text-wrapper-51">IET</div>
+        <div className="text-wrapper-61">BIET</div>
+        <div className="text-wrapper-71">KNIT</div>
+        <div className="text-wrapper-81">AMITY</div>
+        <br></br>
+        <br></br>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+
 
