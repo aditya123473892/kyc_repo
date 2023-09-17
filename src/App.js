5c8d448cda2e3cf76216f82d2668f4dccf0b6eef
y4sh1k4 <mehndirattayashika5@gmail.com>
2023-09-17 21:22:29 +0530

    team and mentorship section

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import {Routes,Route,useNavigationType,useLocation,} from "react-router-dom";
 // import Navbar from './Components/Navbar'
 import Home from './Components/Home';
 import Footer from './Components/Footer'
-
+import Team from './Components/Team'
 import Updates from './Components/home-components/Updates'
 import Navbar from './Components/Navbar'
+import Mentorship from './Components/Mentorship'
 function App() {
   return (
     <div>
        <Navbar></Navbar> 
       <Home></Home>
       <Updates></Updates>
+      <Team></Team> 
       <Footer></Footer>
-      </div>
+      <Mentorship></Mentorship>
+    </div>
     
   )
 }
---
+++ src/Components/Mentorship.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import css from '../Components/mentorship.css'
+export default function mentorship() {
+  return (
+    <div className='mentorship flex_4 col_4'>
+        <div className="frame flex_4">
+            <div className="video_conference flex_4 col_4">
+                <img src="" alt="" />
+                <p>Complete counseling mentorship</p>
+            </div>
+            <div className="building flex_4 col_4">
+                <img src="" alt="" />
+                <p>Get Your College</p>
+            </div>
+            <div className="support flex_4 col_4">
+                <img src="" alt="" />
+                <p>One on One Support</p>
+            </div>
+            <div className="updates flex_4 col_4">
+                <img src="" alt="" />
+                <p>News & Update</p>
+            </div>
+        </div>
+        <div className="benefits flex_4 col_4">
+            <h1>BENEFITS OF <span className="span_4">PERSONAL MENTORSHIP</span></h1>
+            <ul>
+                <li>Direct mentorship from the mentor of the colleges you re targeting</li>
+                <li>Best college preference list according to your rank</li> 
+                <li>Live sessions to resolve your doubts </li>
+                <li>Updates of all the important notices and deadlines of college admissions</li>
+                <li>Most Authentic & Real Life Information of All Colleges. </li>
+            </ul>
+        </div>
+        <div className="enroll_now">
+            <button>ENROLL NOW</button>
+        </div>
+    </div>
+  )
+}
