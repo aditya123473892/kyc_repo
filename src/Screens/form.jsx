646f04b1af9862df67459d25a03e9438b857090c
Anuj Mishra <anujmishraan2005@gmail.com>
2023-09-20 13:52:31 +0530

    New Commit

---
+++ src/Screens/form.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import "./form.css";
+
+export const Form = () => {
+  return (
+    <div className="div25">
+      <div className="class96">
+        <div className="overlap62">
+          <div className="link69">
+            <div className="text-wrapper92">Submit</div>
+          </div>
+          <div className="box">
+      <div className="rectangle1" />
+      <div className="line">
+      <p className="fill-your-details">Fill Your Details &amp; Get Assisted By Team Kyc</p>
+    </div>
+      </div>
+      
+      <div className="name-input">
+      
+      
+        <input
+          type="text"
+          placeholder="Your Name"
+          className="name-input-field"
+        />
+      </div>
+
+      <div className="email-input">
+        <input
+          type="email"
+          placeholder="Your Email Address"
+          className="email-input-field"
+        />
+      </div>
+
+      <div className="mobile-input">
+        <input
+          type="tel"
+          placeholder="Your Mobile Number"
+          className="mobile-input-field"
+        />
+      </div>
+
+      <div className="rank-input">
+        <input
+          type="text"
+          placeholder="Your Rank"
+          className="rank-input-field"
+        />
+      </div>
+        </div>
+        <div className="overlap-group">
+          <p className="we-ll-reach-you-soon">
+            <span className="span">We&#39;ll&nbsp;&nbsp;</span>
+            <span className="text-wrapper-22">
+              reach <br />
+            </span>
+            <span className="span">
+              you soon
+              <br />
+            </span>
+          </p>
+          <div className="help-and-support">
+        <div className="overlap-group-2">
+          <div className="help-support">Help &amp; Support</div>
+          <div className="rectangle">
+          <div className="social-icons">
+        <a href="https://www.facebook.com" target="_blank">
+          <i className="fab fa-facebook"></i>
+        </a>
+        <a href="https://twitter.com" target="_blank">
+          <i className="fab fa-twitter"></i>
+        </a>
+      </div>
+</div>
+</div>
+        </div>
+        </div>
+        </div>
+      </div>
+  );
+};
