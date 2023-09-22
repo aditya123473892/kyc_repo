d571882a024692f16ac8e5ec0136b7e60ed293ac
Anuj Mishra <anujmishraan2005@gmail.com>
2023-09-22 13:28:04 +0530

    New Commit

---
+++ src/Screens/contactus.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './contactus.css'; // Import your CSS file
+
+function Flag() {
+  return (
+    <div className="floating-button">
+      {/* Your button content goes here */}
+      <div class="icon-container">
+        <i class="icon"></i>
+        <span class="text">Contact us</span>
+        </div>
+    </div>
+  );
+}
+
+export default Flag;
