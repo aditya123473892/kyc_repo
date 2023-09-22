807234b3c6c690c264765625895673ae801063eb
Anuj Mishra <anujmishraan2005@gmail.com>
2023-09-22 14:21:48 +0530

    New Commit

---
+++ src/Screens/launchingsoon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './launchingsoon.css'; // Import your CSS file
+
+function Launch() {
+  return (
+    <div className="image-container">
+    </div>
+  );
+}
+
+export default Launch;
